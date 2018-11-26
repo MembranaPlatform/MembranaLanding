3088262ad6021eeb2ddce76a81d739dad5ee453c
Romanist <hello@h-w-h-p.digital>
2018-11-26 13:45:44 +0300

    new top of main page

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -973,3 +973,35 @@ $(document).ready(function () {
     }
   })
 })
+
+// timer
+$(document).ready(function () {
+  var countDownDate = new Date('Jan 1, 2019 00:00:00').getTime()
+  var x = setInterval(function () {
+    // Get todays date and time
+    var now = new Date().getTime()
+
+    // Find the distance between now and the count down date
+    var distance = countDownDate - now
+
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24))
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60))
+    // var seconds = Math.floor((distance % (1000 * 60)) / 1000)
+
+    // Display the result in the element with id="demo"
+    // document.getElementById("demo").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s "
+    $('.top_timer__d .timerJs').html(days)
+    $('.top_timer__h .timerJs').html(hours)
+    $('.top_timer__m .timerJs').html(minutes)
+
+    // If the count down is finished, write some text
+    if (distance < 0) {
+      clearInterval(x)
+      $('.top_timer__d .timerJs').html('00')
+      $('.top_timer__h .timerJs').html('00')
+      $('.top_timer__m .timerJs').html('00')
+    }
+  }, 1000)
+})
