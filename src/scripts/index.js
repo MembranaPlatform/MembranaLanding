04dfff7161f6f75a868f5497a640fde62c7e47a2
Romanist <hello@h-w-h-p.digital>
2018-12-07 15:23:23 +0300

    timer start saling from 00:00 to 18:00

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -972,7 +972,7 @@ $(document).ready(function () {
 
 // timers
 $(document).ready(function () {
-  var countDownDateStart = new Date('Dec 10, 2018 00:00:00 GMT -0000').getTime()
+  var countDownDateStart = new Date('Dec 10, 2018 18:00:00 GMT -0000').getTime()
 
   function padDecimal (number) {
     if (number < 10) number = '0' + number
