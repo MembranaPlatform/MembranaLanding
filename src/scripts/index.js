104858d78814c7fdc82c723b4f7bc7818a54f7e1
Vyacheslav Mychkin <vyacheslav.mychkin@gmail.com>
2019-02-27 21:47:37 +0300

    fixed bonus

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -971,7 +971,7 @@ $(document).ready(function () {
 
 // timers
 $(document).ready(function () {
-  var countDownDateStart = new Date('Feb 27, 2019 18:00:00 GMT+0000').getTime()
+  var countDownDateStart = new Date('Mar 27, 2019 18:00:00 GMT+0000').getTime()
 
   function padDecimal (number) {
     if (number < 10) number = '0' + number
